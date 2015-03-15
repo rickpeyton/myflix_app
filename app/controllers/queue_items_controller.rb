e00bac886bc162f28861595bffe724542e778aff
Rick Peyton <peytorb@gmail.com>
2015-03-15 06:55:35 -0500

    Missed some edge cases on my testing. Working add videos to queue now

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -5,12 +5,17 @@ class QueueItemsController < ApplicationController
   end
 
   def create
-    queue_item = QueueItem.create(
-      user_id: params[:user_id],
-      video_id: params[:video_id]
-    )
-    queue_item.update(position: queue_item.set_queue_position)
-    queue_item.save
+    if QueueItem.where("video_id = ? AND user_id = ?",
+                       params[:video_id], params[:user_id]).exists?
+      existing = QueueItem.where("video_id = ? AND user_id = ?",
+                                 params[:video_id], params[:user_id]).first
+      flash[:danger] = "#{existing.video_title} is already number #{existing.position} your queue."
+    else
+      queue_item = QueueItem.create(user_id: params[:user_id],
+                                    video_id: params[:video_id])
+      queue_item.update(position: queue_item.set_queue_position)
+      queue_item.save
+    end
     redirect_to my_queue_path
   end
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -31,7 +31,8 @@ describe QueueItemsController do
 
   describe "POST #create" do
     context "authenticated user" do
-      it "redirects to the user's my_queue" do
+
+      it "redirects to the my_queue page" do
         user = Fabricate(:user)
         session[:user_id] = user.id
         video = Fabricate(:video)
@@ -39,7 +40,15 @@ describe QueueItemsController do
         expect(response).to redirect_to(my_queue_path)
       end
 
-      it "sets the video to the logged in user's queue_items" do
+      it "creates a queue item" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        video = Fabricate(:video, title: "Polar Express")
+        post :create, video_id: video.id, user_id: user.id
+        expect(QueueItem.count).to eq(1)
+      end
+
+      it "creates a queue item associated with the user" do
         user = Fabricate(:user)
         session[:user_id] = user.id
         video = Fabricate(:video)
@@ -54,9 +63,29 @@ describe QueueItemsController do
         video2 = Fabricate(:video)
         Fabricate(:queue_item, position: 1, user: user, video: video)
         post :create, video_id: video2.id, user_id: user.id
-        expect(user.queue_items.last.position).to eq(2)
+        expect(user.queue_items.find_by(position: 2).video).to eq(video2)
+      end
+
+      it "does not add a video that is already in user queue" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        video = Fabricate(:video)
+        Fabricate(:queue_item, position: 1, user: user, video: video)
+        post :create, video_id: video.id, user_id: user.id
+        expect(user.queue_items.count).to eq(1)
+      end
+
+      it "does add a video that is already in another users queue" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        video = Fabricate(:video)
+        user2 = Fabricate(:user)
+        Fabricate(:queue_item, position: 1, user: user2, video: video)
+        post :create, video_id: video.id, user_id: user.id
+        expect(user.queue_items.count).to eq(1)
       end
     end
+
      context "non-authenticated user" do
        it "redirects to the sign-in page of not logged in" do
          session[:user_id] = nil
