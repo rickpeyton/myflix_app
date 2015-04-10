8be4800e6f810aa2821d9416eead2c09a191c541
Rick Peyton <peytorb@gmail.com>
2015-04-09 21:40:48 -0500

    completed relationships setup

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,4 +4,10 @@ class RelationshipsController < ApplicationController
   def index
     @relationships = current_user.following_relationships
   end
+
+  def destroy
+    relationship = Relationship.find(params[:id])
+    relationship.destroy if current_user == relationship.follower
+    redirect_to people_path
+  end
 end
--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -14,6 +14,40 @@ describe RelationshipsController do
     it_behaves_like "redirect_to_sign_in" do
       let(:action) { get :index }
     end
+  end
 
+  describe "DELETE #destroy" do
+    it_behaves_like "redirect_to_sign_in" do
+      let(:action) { delete :destroy, id: 1 }
+    end
+
+    it "redirects to the people path" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      bob = Fabricate(:user)
+      relationship = Fabricate(:relationship, leader: bob, follower: alice)
+      delete :destroy, id: relationship
+      expect(response).to redirect_to people_path
+    end
+
+    it "destroys the relationship if the current user is the follower" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      bob = Fabricate(:user)
+      relationship = Fabricate(:relationship, leader: bob, follower: alice)
+      delete :destroy, id: relationship
+      expect(Relationship.all.count).to eq(0)
+    end
+
+
+    it "does not destroy the relationship if the current user is NOT the follower" do
+      alice = Fabricate(:user)
+      set_current_user(alice)
+      bob = Fabricate(:user)
+      charles = Fabricate(:user)
+      relationship = Fabricate(:relationship, leader: bob, follower: charles)
+      delete :destroy, id: relationship
+      expect(Relationship.all.count).to eq(1)
+    end
   end
 end
