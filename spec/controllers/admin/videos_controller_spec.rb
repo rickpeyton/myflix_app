fb6dd6624402611f979a3fde5b84f0e62d942f1d
Rick Peyton <peytorb@gmail.com>
2015-04-29 05:49:43 -0500

    Admin can add a new video sans uploads

--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -30,4 +30,62 @@ describe Admin::VideosController do
       expect(flash[:danger]).not_to be_nil
     end
   end
+
+  describe "POST#create" do
+    it_behaves_like "redirect_to_sign_in" do
+      let(:action) { post :create }
+    end
+
+    it_behaves_like "requires admin" do
+      let(:action) { post :create }
+    end
+
+    context "with valid input" do
+      before do
+        set_current_admin
+      end
+
+      it "redirects to the add video page" do
+        category = Fabricate(:category)
+        post :create, video: { title: "Video Title", description: "Video Description", category_id: category.id }
+        expect(response).to redirect_to new_admin_video_path
+      end
+
+      it "creates a new video" do
+        category = Fabricate(:category)
+        post :create, video: { title: "Video Title", description: "Video Description", category_id: category.id }
+        expect(Video.all.count).to eq(1)
+      end
+
+      it "sets the flash message" do
+        category = Fabricate(:category)
+        post :create, video: { title: "Video Title", description: "Video Description", category_id: category.id }
+        expect(flash[:success]).not_to be_nil
+      end
+    end
+
+    context "with invalid input" do
+      before do
+        set_current_admin
+      end
+
+      it "renders the new template" do
+        category = Fabricate(:category)
+        post :create, video: { description: "Video Description", category_id: category.id }
+        expect(response).to render_template :new
+      end
+
+      it "sets the video" do
+        category = Fabricate(:category)
+        post :create, video: { description: "Video Description", category_id: category.id }
+        expect(:video).to be_present
+      end
+
+      it "sets the flash message" do
+        category = Fabricate(:category)
+        post :create, video: { description: "Video Description", category_id: category.id }
+        expect(flash[:danger]).to be_present
+      end
+    end
+  end
 end
