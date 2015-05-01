2aa0e6e0dbb80d958d43b3a0b128321a31a78576
Rick Peyton <peytorb@gmail.com>
2015-05-01 05:13:27 -0500

    Messed up the host I believe

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.staging? || Rails.env.production?
     config.storage    = :aws
     config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
     config.aws_acl    = :public_read
-    config.asset_host = 'https://rickflix.herokuapp.com'
+    config.asset_host = 'https://s3.amazonaws.com/myflix-cmqzlnwe'
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
     config.aws_credentials = {
